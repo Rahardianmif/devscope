8075cfffba676cb224d7375816e038b2ffd37e22
rahardianmif <rahardianmuhamadmiftahudin@gmail.com>
2026-06-17 16:52:56 +0700

    Initial project setup

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="container">
+      <h1>DevScope</h1>
+      <p>Analyze GitHub profile instantly.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/routes/Index.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home/Home";
+import Dashboard from "../pages/Dashboard/Dashboard";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard/:username" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
